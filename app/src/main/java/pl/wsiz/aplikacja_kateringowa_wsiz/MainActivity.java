780ba1843b99de9fa9cfb76700a351c68f0a3da1
Ada Skrzypiec <adriannaskrzypiec@o2.pl>
2020-06-12 20:16:25 +0200

    edycja bibliotek

--- app/src/main/java/pl/wsiz/aplikacja_kateringowa_wsiz/MainActivity.java
+++ app/src/main/java/pl/wsiz/aplikacja_kateringowa_wsiz/MainActivity.java
@@ -1,14 +1,83 @@
 package pl.wsiz.aplikacja_kateringowa_wsiz;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
+import com.google.firebase.FirebaseAppLifecycleListener;
+import dmax.dialog.SpotsDialog;
+import io.reactivex.disposables.CompositeDisposable;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int APP_REQUEST_CODE = 7171;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener listener;
+    private AlertDialog dialog;
+    private CompositeDisposable disposable = new CompositeDisposable();
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseAuth.addAuthStateListener(listener);
+    }
+
+    @Override
+    protected void onStop() {
+        if (listener != null)
+            firebaseAuth.removeAuthStateListener(listener);
+        disposable.clear();
+        super.onStop();
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Init();
+    }
+
+    private void Init() {
+        firebaseAuth = FirebaseAuth.getInstance();
+        dialog = new SpotsDialog.Builder().setCancelable(false).setContext(this).build();
+        listener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                if (user != null) {
+                    //Account is already logged in
+                    CheckUserFromFirebase(user);
+                } else {
+                    phoneLogIn();
+                }
+            }
+        };
+    }
+
+    private void CheckUserFromFirebase(FirebaseUser user) {
+    }
+
+    private void phoneLogIn() {
+
+        // startActivityForResult(intent,APP_REQUEST_CODE);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == APP_REQUEST_CODE) {
+
+        }
     }
 }
+
